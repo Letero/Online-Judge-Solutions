b3f66b991d7482bfaddbd70f07096c22cd715274
Kurzynski Jaroslaw <jaroslaw.kurzynski@tieto.com>
2018-08-31 10:25:11 +0200

    SPOJ

--- SPOJ/Classical/39_PIGBANK_Piggy_bank.c
+++ SPOJ/Classical/39_PIGBANK_Piggy_bank.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
 
-struct Money_t
+int main() // 0/1 knapsack problem
 {
-    int value;
-    int weight;
-};
-
-int getLowest(struct Money_t *money, int size, int piggyWeight)
-{
-    int min;
-    for (int i = 0; i < size; ++i)
-    {
-        int temp = piggyWeight / money[i].weight;
-        }
-    return -1;
-}
-
-int main(void)
-{
-    int tests, empty, filled, coinsNo, value, weight;
+    int tests, empty, full, N, p[500], w[500], dp[10000];
     scanf("%d", &tests);
 
     while (tests--)
     {
-        scanf("%d%d%d", &empty, &filled, &coinsNo);
-        struct Money_t money[coinsNo];
+        scanf("%d %d %d", &empty, &full, &N);
 
-        for (int i = 0; i < coinsNo; ++i)
+        for (int i = 0; i < N; ++i)
         {
-            scanf("%d%d", &money[i].value, &money[i].weight);
+            scanf("%d %d", &p[i], &w[i]);
         }
-        int result = getLowest(money, coinsNo, filled - empty);
 
-        if (result > 0)
+        full -= empty;
+        dp[0] = 0;
+
+        for (int i = 1; i <= full; ++i)
         {
-            printf("The minimum amount of money in the piggy bank is %d.\n", result);
+            dp[i] = -1;
+
+            for (int j = 0; j < N; ++j)
+            {
+                if ((i >= w[j] && dp[i - w[j]] != -1) && (dp[i] == -1 || p[j] + dp[i - w[j]] <= dp[i]))
+                {
+                    dp[i] = p[j] + dp[i - w[j]];
+                }
+            }
         }
-        else
+
+        if (dp[full] == -1)
             printf("This is impossible.\n");
+        else
+            printf("The minimum amount of money in the piggy-bank is %d.\n", dp[full]);
     }
 
     return 0;
