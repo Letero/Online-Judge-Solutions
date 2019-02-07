b46364e3efb53de1ae754245b113f36c4bc86683
Kurzynski Jaroslaw <jaroslaw.kurzynski@tieto.com>
2019-02-07 11:17:10 +0100

    CodeWars

---
+++ CodeWars/TwoSum.cpp
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/cpp
+std::pair<std::size_t, std::size_t> two_sum(const std::vector<int>& numbers, int target) 
+{
+  for (int i = 0; i < numbers.size(); ++i)
+  {
+    for (int j = i + 1; j < numbers.size(); ++j)
+    {
+      if ((numbers[i] + numbers[j]) == target)
+      {
+        return {i, j};  
+      }
+    }
+  }
+}
