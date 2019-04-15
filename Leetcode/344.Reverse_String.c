02f636267346356bbf6630c088d18fd45973b8dc
Kurzynski Jaroslaw <jaroslaw.kurzynski@tieto.com>
2019-04-15 14:31:53 +0200

    LeetCode

---
+++ Leetcode/344.Reverse_String.c
@@ -0,0 +1,9 @@
+void reverseString(char* s, int sSize)
+{
+    for (int i = 0, j = sSize-1; i < sSize/2; ++i, --j)
+    {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
