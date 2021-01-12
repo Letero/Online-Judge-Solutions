2f9f4a8a7246e1b69dc4501f31f91ee954aff6f3
Jaroslaw Kurzynski <jaroslaw.kurzynski@huuugegames.com>
2021-01-12 22:24:07 +0100

    Leetcode update

--- Leetcode/344.Reverse_String.c
+++
@@ -1,9 +0,0 @@
-void reverseString(char *s, int sSize)
-{
-    for (int i = 0, j = sSize - 1; i < sSize / 2; ++i, --j)
-    {
-        char temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-    }
-}
